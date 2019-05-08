c75a96d83d73278288a8304ae7a8995c80eb328d
tykazzz <tlakomy2001@gmail.com>
2019-05-09 00:14:27 +0200

    improve Test4_pop/2.4

--- Test4_pop/2.4.py
+++ Test4_pop/2.4.py
@@ -11,13 +11,11 @@ def Floor(x):
 	return x
 
 def Digit(x,k):
-	if k<0:
-		while k!=0:
-			x=x*10
-			k+=1
-	elif k>0:
-		while k!=1:
-			x=x/10
-			k-=1
+	while k<0:
+		x=x*10
+		k+=1
+	while k>0:
+		x=x/10
+		k-=1
 	x = Floor(x)%10
 	return x
